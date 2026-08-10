0530ea832105063046b76c1e962317b5b6838493
ramiluna <ramiluna73@gmail.com>
2026-08-09 16:26:59 -0800

    Ejercicio A2 completo y funcionando

---
+++ Parte_A/Ejercicio_A2.py
@@ -0,0 +1,10 @@
+cantidad= "5"
+precio= 100
+resultado_No_Esperado=cantidad*precio 
+print (resultado_No_Esperado)
+#Correccion del codigo, ya que el resultado es el numero 5 repetido 100 veces y no un calculo matematico. 
+resultado_Esperado= int(cantidad)*precio 
+print ("\nResultado Esperado:")
+print (resultado_Esperado)
+
+
