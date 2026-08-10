49997cbcf799b0905dadba6f162fab203d138031
ramiluna <ramiluna73@gmail.com>
2026-08-09 22:37:18 -0800

    Ejercicio C8 completo y funcionando

---
+++ Parte_C/Ejercicio_C8.py
@@ -0,0 +1,15 @@
+inventario = {"manzana":{
+    "precio": 2,
+    "stock": 20
+},
+"banana": {
+    "precio": 5,
+    "stock": 30
+}, 
+"tomate":{
+    "precio": 10,
+    "stock": 100
+}
+}
+print(inventario["manzana"]["precio"])
+print(inventario ["tomate"]["stock"])
