9dc2b60642f964dc06a9170ad9e0a826d1dc671a
ramiluna <ramiluna73@gmail.com>
2026-08-09 19:33:37 -0800

    Ejercicio B1 completo y funcionando

---
+++ Parte_B/Ejercicio_B1.py
@@ -0,0 +1,11 @@
+# 1. Defino la playlist como una lista. 
+playlist=["El terco", "Cruz diablo!","No more teers","Salando las heridas","Ji ji ji"]
+# 2. Imprimo todas las canciones disponibles en la lista.
+print("\n Playlist entera")
+print (playlist)
+# 3. Imprimo la primer cancion que aparece en la lista
+print("\nPrimer cancion de la playlist:")
+print(playlist[0])
+# 4. Imprimo la ultima cancion de la lista.
+print("\nUltima cancion de la playlist:")
+print(playlist[-1])
