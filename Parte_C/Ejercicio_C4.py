8021f46303d30897549fcee4e44366d611b0af75
ramiluna <ramiluna73@gmail.com>
2026-08-09 22:01:06 -0800

    Ejercicio C4 completo y funcionando

---
+++ Parte_C/Ejercicio_C4.py
@@ -0,0 +1,20 @@
+peliculas = [
+    {
+        "titulo": "Interestelar",
+        "anio": 2014,
+        "director": "Christopher Nolan"
+    },
+    {
+        "titulo": "Jurassic Park",
+        "anio": 1993,
+        "director": "Steven Spielberg"
+    },
+    {
+        "titulo": "Toy Story",
+        "anio": 1995,
+        "director": "John Lasseter"
+    }
+]
+
+for pelicula in peliculas:
+    print(pelicula["titulo"])
